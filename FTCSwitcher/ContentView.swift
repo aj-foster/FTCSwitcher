390c744747b2d434f1ed4e2a71a63175eea4e677
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-12-03 23:02:56 -0500

    Initial Commit

---
+++ FTCSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FTCSwitcher
+//
+//  Created by AJ Foster on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FTCSwitcher/FTCSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTCSwitcherApp.swift
+//  FTCSwitcher
+//
+//  Created by AJ Foster on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FTCSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
