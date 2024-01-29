80b78ea7a8e73d174f9bd64a20ac86b48058b046
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-01-29 16:29:48 -0500

    Add Sparkle for future updates

--- FTCSwitcher/FTCSwitcherApp.swift
+++ FTCSwitcher/FTCSwitcherApp.swift
@@ -5,10 +5,17 @@
 //  Created by AJ Foster on 12/3/23.
 //
 
+import Sparkle
 import SwiftUI
 
 @main
 struct FTCSwitcherApp: App {
+//    private let updaterController: SPUStandardUpdaterController
+    
+//    init() {
+//        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+//    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView(scoring: Scoring.current, switcher: Switcher.current)
