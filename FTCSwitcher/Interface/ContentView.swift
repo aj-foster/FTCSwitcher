42c80ebb50aa4c1d7552777d77cda96184ced48d
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-02-16 02:55:32 -0500

    Split division settings view and use grid to show multiple divisions

--- FTCSwitcher/Interface/ContentView.swift
+++ FTCSwitcher/Interface/ContentView.swift
@@ -4,11 +4,32 @@ struct ContentView: View {
     @AppStorage("divisionCount") private var division_count = 1;
     
     var body: some View {
+        let _ = Self._printChanges()
+        
         VStack {
-            HStack {
-                ForEach(1...division_count, id: \.self) { division in
-                    DivisionView()
-                    if division < division_count { Divider() }
+            Picker("", selection: $division_count) {
+                Text("1 Division").tag(1)
+                Text("2 Divisions").tag(2)
+                Text("3 Divisions").tag(3)
+                Text("4 Divisions").tag(4)
+            }.frame(width: 200, alignment: .leading).padding(20)
+            Grid {
+                GridRow {
+                    DivisionView(division: 1).frame(minWidth: 550).padding(20)
+                    if division_count > 1 {
+                        HStack { Divider() }
+                        DivisionView(division: 2).frame(minWidth: 550).padding(20)
+                    }
+                }
+                if division_count > 2 {
+                    Divider()
+                    GridRow {
+                        DivisionView(division: 3).frame(minWidth: 550).padding(20)
+                        if division_count > 3 {
+                            HStack { Divider() }
+                            DivisionView(division: 4).frame(minWidth: 550).padding(20)
+                        }
+                    }
                 }
             }
             Text("Created by [FTAJ](https://youtube.com/@FTAAJ). If this application helped you, please consider making a [donation](https://donate.stripe.com/eVa8A63oQ0drcF2aEE) to the Orlando Robotics Foundation.")
@@ -16,7 +37,7 @@ struct ContentView: View {
                 .fixedSize(horizontal: false, vertical: true)
                 .frame(width: 320)
                 .font(.system(size: 10))
-                .padding([.bottom], 20)
+                .padding([.top, .bottom], 20)
         }
     }
 }
--- FTCSwitcher/Interface/DivisionView.swift
+++ FTCSwitcher/Interface/DivisionView.swift
@@ -1,100 +1,29 @@
 import SwiftUI
 
 struct DivisionView: View {
+    private var division: Int
     @ObservedObject var scoring: Scoring
     @ObservedObject var switcher: Switcher
     
-    @AppStorage("scoringCode") private var scoring_code = ""
-    @AppStorage("scoringHost") private var scoring_host = "localhost"
-    @AppStorage("switcherUrl") private var switcher_url = ""
-    
     @AppStorage("fieldCount") private var field_count = 1
-    @AppStorage("finalsField") private var finals_field = 1
     @AppStorage("reverseFields") private var reverse_fields = true
     
     @State private var showScoringHostHelp = false
     @State private var showScoringCodeHelp = false
     @State private var showSwitcherURLHelp = false
     
-    init() {
+    init(division: Int) {
         scoring = Scoring()
         switcher = Switcher()
+        self.division = division
     }
     
     var body: some View {
+        let _ = Self._printChanges()
+        
         VStack {
-        HStack(alignment: .top, spacing: 40) {
-            Form {
-                Section(header: Text("Scoring System").bold()) {
-                    HStack {
-                        TextField(text: $scoring_host, prompt: Text("Hostname (ex. localhost)")) {
-                            Text("Host")
-                        }
-                        Help(text: "Use \"localhost\" if the scoring system runs on the same computer, or the IP address displayed at the top of the scoring system otherwise.", width: 290)
-                    }
-                    HStack {
-                        TextField(text: $scoring_code, prompt: Text("Event Code (ex. florlm1)")) {
-                            Text("Event")
-                        }
-                        Help(text: "Event code (ex. \"usflorls\"), which usually appears in the URL of the event page.", width: 280)
-                    }.padding([.bottom], 5)
-                    HStack {
-                        Button(scoring.state == .connected ? "Disconnect" : "Connect") {
-                            if scoring.state == .disconnected {
-                                scoring.connect(hostname: scoring_host, event_code: scoring_code)
-                            } else {
-                                scoring.disconnect()
-                            }
-                        }
-                        Text(scoring.error ?? " ")
-                    }
-                }
-                Spacer().frame(height: 40)
-                Section(header: Text("ATEM Switcher").bold()) {
-                    HStack {
-                        TextField(text: $switcher_url, prompt: Text("Blank for USB")) {
-                            Text("URL")
-                        }
-                        Help(text: "Leave blank if the ATEM switcher is connected via USB. Otherwise, input the IP address of the switcher.", width: 250)
-                    }.padding([.bottom], 5)
-                    HStack {
-                        Button(switcher.state == .connected ? "Disconnect" : "Connect") {
-                            if switcher.state == .disconnected {
-                                switcher.connect(switcher_url)
-                            } else {
-                                switcher.disconnect()
-                            }
-                        }
-                        Text(switcher.error ?? " ")
-                    }
-                }
-            }
-            Form {
-                Section(header: Text("Setup").bold()) {
-                    HStack {
-                        TextField(value: $field_count, format: .number, prompt: Text("Number of playing fields")) {
-                            Text("Field Count")
-                        }
-                        Stepper(value: $field_count, in: 1...4, step: 1) {}.labelsHidden()
-                        Help(text: "Must match the number of fields configured in the scoring software.", width: 240)
-                    }
-                    HStack {
-                        TextField(value: $finals_field, format: .number, prompt: Text("Field number")) {
-                            Text("Finals Field")
-                        }
-                        Stepper(value: $finals_field, in: 1...field_count, step: 1) {}.labelsHidden()
-                        Help(text: "For events with elimination matches, which field will hold the finals. Usually field 1.", width: 200)
-                    }.padding([.bottom], 4)
-                    HStack {
-                        Picker("Orientation", selection: $reverse_fields) {
-                            Text("Audience").tag(false)
-                            Text("Scoring").tag(true)
-                        }.disabled(field_count <= 1)
-                        Help(text: "Changes the order of the fields in the interface below.", width: 200)
-                    }
-                }
-            }
-        }.padding(40)
+            DivisionSettingsView(division: division, scoring: scoring, switcher: switcher)
+                .padding([.bottom], 20)
         
             Form {
                 Section() {
@@ -133,14 +62,9 @@ struct DivisionView: View {
                 }
             }
         }
-        .padding([.bottom], 20)
-        .padding([.leading, .trailing], 40)
-        .onChange(of: $field_count.wrappedValue) { new_field_count in
-            finals_field = min(finals_field, new_field_count)
-        }
     }
 }
 
 #Preview("FTC Switcher") {
-    DivisionView()
+    DivisionView(division: 1)
 }
