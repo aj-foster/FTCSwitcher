b7073c14d2bfb75a90e8eb4f717d6b7234efb89a
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2024-03-24 15:47:21 -0400

    Attempt to improve draw performance and spacing of items

--- FTCSwitcher/Interface/FieldView.swift
+++ FTCSwitcher/Interface/FieldView.swift
@@ -19,37 +19,26 @@ struct FieldView: View {
     
     var body: some View {
         let _ = Self._printChanges()
+        let fields = if reverse_fields { Array((1...field_count).reversed()) } else { Array(1...field_count) }
         
         Form {
             Section() {
-                Grid(alignment: .leading, horizontalSpacing: 40, verticalSpacing: 10) {
+                Grid(alignment: .leading, horizontalSpacing: 20, verticalSpacing: 5) {
                     GridRow {
                         HStack {
                             Text("Macros").bold()
                             Help(text: "Number of the pre-programmed macro to use during each event. Use 0 to take no action.")
                         }
-                        if reverse_fields {
-                            ForEach((1...field_count).reversed(), id: \.self) { index in
-                                Text("Field \(index)").bold()
-                            }
-                        } else {
-                            ForEach((1...field_count), id: \.self) { index in
-                                Text("Field \(index)").bold()
-                            }
+                        ForEach(fields, id: \.self) { index in
+                            Text("Field \(index)").bold()
                         }
                     }
-                    .padding([.bottom], 10)
+                    .padding([.bottom], 5)
                     ForEach(ScoringEvents) { event in
                         GridRow {
                             Text(event.title)
-                            if reverse_fields {
-                                ForEach((1...field_count).reversed(), id: \.self) { field in
-                                    MacroSetting("d\(division)field\(field)\(event.macro)Macro", switcher: switcher)
-                                }
-                            } else {
-                                ForEach((1...field_count), id: \.self) { field in
-                                    MacroSetting("d\(division)field\(field)\(event.macro)Macro", switcher: switcher)
-                                }
+                            ForEach(fields, id: \.self) { field in
+                                MacroSetting("d\(division)field\(field)\(event.macro)Macro", switcher: switcher)
                             }
                         }
                     }
